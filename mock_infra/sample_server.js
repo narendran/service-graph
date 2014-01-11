51d8b8392293e854d1e71c0fa6733dcf8069c852
Anup Chenthamarakshan <anupcshan@gmail.com>
2014-01-10 20:34:20 -0800

    Calculating own statistics.

--- mock_infra/sample_server.js
+++ mock_infra/sample_server.js
@@ -2,25 +2,76 @@ var express = require('express');
 var app = express();
 var sleep = require('sleep');
 
-var STATS_REFRESH_INTERVAL_MSEC = 1000;
+var STATS_REFRESH_INTERVAL_MSEC = 2000;
 
-var stats = {};
+var ownStats = {
+  'calls': 0,
+  'total_resp_ms': 0,
+  'errors': 0
+};
+
+var childStats = {};
+var logEntries = [];
+
+function purgeOldLogs() {
+  var currentTime = new Date();
+  var deadline = currentTime - STATS_REFRESH_INTERVAL_MSEC;
+  while (logEntries.length > 0) {
+    var logEntry = logEntries[0];
+    if (logEntry.timestamp >= deadline) {
+      break;
+    }
+
+    ownStats.calls --
+    ownStats.total_resp_ms -= logEntry.resp_ms;
+    ownStats.errors -= logEntry.error;
+
+    // TODO: Remove child stats for logEntry
+
+    logEntries.shift();   // Remove from head of queue
+  }
+}
 
 function statsRefresher() {
+  purgeOldLogs();
+  console.log(ownStats);
+}
+
+function logRequest(resp_ms, child_requests, error) {
+  var logEntry = {
+    'timestamp': new Date(),
+    'resp_ms': resp_ms,
+    'children': child_requests,
+    'error': error
+  };
+
+  logEntries.push(logEntry);
+  ownStats.calls ++;
+  ownStats.total_resp_ms += resp_ms;
+  ownStats.errors += error;
+
+  // TODO: Add child stats for logEntry
 }
 
 app.use(function(req, res, next){
   var timer_start = new Date();
   next();
   var timer_end = new Date();
-  console.log(timer_end - timer_start);
+  var selftime = timer_end - timer_start;
+  console.log('Response time: ', selftime, 'ms');
+  logRequest(selftime, [], false);
 });
 
 app.get('/', function(req, res){
   res.send('Hello world');
-  // sleep.usleep(3000);
+
+  // Fake 10ms wait time.
+  sleep.usleep(10000);
 });
 
 setInterval(statsRefresher, STATS_REFRESH_INTERVAL_MSEC);
+
+// The server is about to start up
+// TODO: Send a hello to the message queue with my configuration.
 
 app.listen(3000);
