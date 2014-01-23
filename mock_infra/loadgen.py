6b652292428417200670438b42f9a1eb6a8aebe1
Anup Chenthamarakshan <anupcshan@gmail.com>
2014-01-23 12:37:31 -0800

    Renamed sample configs.

--- mock_infra/loadgen.py
+++ mock_infra/loadgen.py
@@ -5,4 +5,6 @@ import urllib
 import time
 
 while True:
-  urllib.urlopen('http://localhost:3500/HelloService')
+  urllib.urlopen('http://localhost:3500/PreferencesService')
+  urllib.urlopen('http://localhost:4321/WebStoreService')
+  urllib.urlopen('http://localhost:5000/EmailService')
